5b6793804749c47730a1abdf47a4bcb12f6b4a21
elekmad <damien.warin@gmail.com>
2020-08-30 20:45:17 +0200

    Ajout de l'interaction clavier

--- Triangles/Outside.c
+++ Triangles/Outside.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL_keycode.h>
 #include <math.h>
 #include <time.h>
 
@@ -22,10 +23,10 @@ void point_add_vector(double *out, double *point1, double *point2, int geometric
     else
     {
 	double vector_size = sqrt(square(point2[0] - point1[0]) + square(point2[1] - point1[1]));
-	printf("vector size %f points %f %f %f %f distance %f", vector_size, point1[0], point1[1], point2[0], point2[1], distance);
+//	printf("vector size %f points %f %f %f %f distance %f", vector_size, point1[0], point1[1], point2[0], point2[1], distance);
         out[0] = point1[0] + ((point2[0] - point1[0]) / vector_size) * distance;
         out[1] = point1[1] + ((point2[1] - point1[1]) / vector_size) * distance;
-	printf(" => out %f %f\n", out[0], out[1]);
+//	printf(" => out %f %f\n", out[0], out[1]);
     }
 }
 
@@ -61,7 +62,7 @@ void draw_triangles(SDL_Renderer *renderer, double points[6], int geometric, dou
 
 int main( int argc, char *argv[ ] )
 {
-    int animated = 1, nb_triangles_max = 30, geometric = 0, distance = -15;
+    int animated = 0, nb_triangles_max = 0/*35*/, geometric = 0, distance = -5;
     SDL_Window *window;
     if( SDL_Init( SDL_INIT_VIDEO ) == -1 )
     {
@@ -97,20 +98,9 @@ int main( int argc, char *argv[ ] )
     const Uint8 *keyboard = SDL_GetKeyboardState(NULL);
     SDL_Event event;
     unsigned int current_time = SDL_GetTicks(), nb_triangles = nb_triangles_max, triangles_speed = 1;;
-    double center_x = SCREEN_WIDTH / 2, center_y = SCREEN_HEIGTH / 2, distance_from_center = (SCREEN_WIDTH < SCREEN_HEIGTH ? SCREEN_WIDTH : SCREEN_HEIGTH) * 0.2, size = 50;//(SCREEN_WIDTH < SCREEN_HEIGTH ? SCREEN_WIDTH : SCREEN_HEIGTH) * 0.5;
-    for(int num_triangle = 0; num_triangle < 6; num_triangle++)
-    {
-        double triangle[6] = {center_x, center_y,
-				center_x + size * cos(num_triangle * M_PI / 3), center_y + size * sin(num_triangle * M_PI / 3),
-				center_x + size * cos((num_triangle + 1) * M_PI / 3), center_y + size * sin((num_triangle + 1) * M_PI / 3)};
-	for(int cmpt = 0; cmpt < 3; cmpt++)
-	{
-	    triangle[2*cmpt] += distance_from_center * cos(M_PI/6 + num_triangle * M_PI / 3);
-	    triangle[2*cmpt+1] += distance_from_center * sin(M_PI/6 + num_triangle * M_PI / 3);
-	}
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        draw_triangles(renderer, triangle, geometric, distance, nb_triangles, num_triangle % 2);
-    }
+    int redraw = 1;//Draw at least 1 time !
+    double center_x = SCREEN_WIDTH / 2, center_y = SCREEN_HEIGTH / 2, distance_from_center = (SCREEN_WIDTH < SCREEN_HEIGTH ? SCREEN_WIDTH : SCREEN_HEIGTH) * 0.1, size = 50, angle = 0;
+
     while(1)
     {
 	if(animated)
@@ -124,28 +114,33 @@ int main( int argc, char *argv[ ] )
   	        if(nb_triangles < 1)
   	    	    triangles_speed = 1;
   	           
-  	        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  	        pos.x = 0;
-  	        pos.y = 0;
-  	        pos.w = SCREEN_WIDTH;
-  	        pos.h = SCREEN_HEIGTH;
-  	        SDL_RenderClear(renderer);
-  	        SDL_RenderFillRect(renderer, &pos);
-                for(int num_triangle = 0; num_triangle < 6; num_triangle++)
-                {
-                    double triangle[6] = {center_x, center_y,
-            				center_x + size * cos(num_triangle * M_PI / 3), center_y + size * sin(num_triangle * M_PI / 3),
-            				center_x + size * cos((num_triangle + 1) * M_PI / 3), center_y + size * sin((num_triangle + 1) * M_PI / 3)};
-                    for(int cmpt = 0; cmpt < 3; cmpt++)
-                    {
-                        triangle[2*cmpt] += distance_from_center * cos(M_PI/6 + num_triangle * M_PI / 3);
-                        triangle[2*cmpt+1] += distance_from_center * sin(M_PI/6 + num_triangle * M_PI / 3);
-                    }
-                    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-                    draw_triangles(renderer, triangle, geometric, distance, nb_triangles, num_triangle % 2);
-                }
- 	    
+		redraw = 1;
   	    }  
+	}
+	if(redraw)
+	{
+	    redraw = 0;
+	    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            pos.x = 0;
+            pos.y = 0;
+            pos.w = SCREEN_WIDTH;
+            pos.h = SCREEN_HEIGTH;
+            SDL_RenderClear(renderer);
+            SDL_RenderFillRect(renderer, &pos);
+            for(int num_triangle = 0; num_triangle < 6; num_triangle++)
+            {
+                double triangle[6] = {center_x + size * cos(M_PI / 2  + num_triangle * M_PI / 3 + angle), center_y + size * sin(M_PI / 2 + num_triangle * M_PI / 3 + angle),
+                                    center_x + size * cos(num_triangle * M_PI / 3 - M_PI / 6 + angle), center_y + size * sin(num_triangle * M_PI / 3 - M_PI / 6 + angle),
+                                    center_x + size * cos(num_triangle * M_PI / 3 + 7 * M_PI / 6 + angle), center_y + size * sin(num_triangle  * M_PI / 3 + 7 * M_PI / 6 + angle)};
+//printf("centerx %f centery %f size %f angle %f\n\tnum triangle %d\n\t{%f %f\n\t%f %f\n\t%f %f}\n", center_x, center_y, size, angle, num_triangle, triangle[0], triangle[1], triangle[2], triangle[3], triangle[4], triangle[5]);
+                for(int cmpt = 0; cmpt < 3; cmpt++)
+                {
+                    triangle[2*cmpt] += distance_from_center * cos(M_PI/6 + num_triangle * M_PI / 3);
+                    triangle[2*cmpt+1] += distance_from_center * sin(M_PI/6 + num_triangle * M_PI / 3);
+                }
+                SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+                draw_triangles(renderer, triangle, geometric, distance, nb_triangles, num_triangle % 2);
+            }
 	}
         SDL_PumpEvents();
         if(keyboard[SDL_SCANCODE_ESCAPE])
@@ -162,6 +157,59 @@ int main( int argc, char *argv[ ] )
                     SDL_Quit();
                     return EXIT_SUCCESS;// Quit the program
                     break;
+                case SDL_KEYDOWN:
+		    {
+			SDL_Keymod modstate = SDL_GetModState();
+                        printf("keydown %d modstate %d\n", event.key.keysym.sym, modstate);
+                        switch(event.key.keysym.sym)
+                        {
+                        case SDLK_g:
+			    geometric = 1 - geometric;
+			    redraw = 1;
+			    break;
+                        case SDLK_n:
+			    if(modstate & KMOD_SHIFT)
+				nb_triangles++;
+			    else
+				nb_triangles--;
+			    redraw = 1;
+                            break;
+                        case SDLK_c:
+			    if(modstate & KMOD_SHIFT)
+				distance_from_center++;
+			    else
+				distance_from_center--;
+			    redraw = 1;
+                            break;
+                        case SDLK_a:
+			    if(modstate & KMOD_SHIFT)
+				angle += M_PI / 10;
+			    else
+				angle -= M_PI / 10;
+			    redraw = 1;
+                            break;
+                        case SDLK_d:
+			    if(modstate & KMOD_SHIFT)
+				distance++;
+			    else
+				distance--;
+			    redraw = 1;
+                            break;
+                        case SDLK_s:
+			    if(modstate & KMOD_SHIFT)
+				size++;
+			    else
+				size--;
+			    redraw = 1;
+                            break;
+                        case SDLK_ESCAPE:
+                            SDL_Quit();
+                            return EXIT_SUCCESS;// Quit the program
+                            break;
+                        }
+			printf("nb triangles = %d, distance = %d size = %f, distance from center %f, angle %f\n", nb_triangles, distance, size, distance_from_center, angle);
+                    break;
+		}
             }
         }
 
